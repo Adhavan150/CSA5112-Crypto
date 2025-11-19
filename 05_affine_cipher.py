e667301c7129eac889dde56f3ffbebcef87fdcda
ADHAVAN M G <adhavangnanasekar@gmail.com>
2025-11-19 13:32:03 +0530

    Update 05_affine_cipher.py

--- 05_affine_cipher.py
+++ 05_affine_cipher.py
@@ -48,3 +48,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+OUTPUT:
+=== Affine Cipher ===
+Valid 'a': 1,3,5,7,9,11,15,17,19,21,23,25
+'e' for encrypt, 'd' for decrypt: e
+Enter text: Adhavan
+Enter a: 5
+Enter b: 7
+Encrypted: HWQHIHU
